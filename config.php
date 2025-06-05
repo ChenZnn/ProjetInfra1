d0dad677e0bcfe6e334b790713b8d347fee36954
ChenZnn <theo.noel2109@gmail.com>
2025-06-05 13:55:33 +0200

    Modif config

--- config.php
+++ config.php
@@ -1,4 +1,3 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=upload_demo;charset=utf8', 'root', '');
+$pdo = new PDO("mysql:host=localhost;dbname=projetinfra1;charset=utf8", "projetuser", "motdepassefort");
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
